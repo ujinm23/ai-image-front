7c09fc3fa0db81367d30c059e9edade415bc4a6f
Ujin <ujin@Ujins-MacBook-Pro.local>
2025-12-23 21:29:26 +0800

    pppp

--- src/app/_features/imageEditing.tsx
+++ src/app/_features/imageEditing.tsx
@@ -2,11 +2,11 @@
 import { useState } from "react";
 import { ArticleGeneratorIcon } from "../_icons/ArticleGeneratorIcon";
 import ReloadButton from "../_components/reloadButton";
-import FileUpload from "@/app/ImageEditor/FileUpload";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { ImageIcon } from "../_icons/ImageIcon";
+import DeleteButton from "@/app/_components/DeleteButton";
 
 export default function ImageEditing() {
   const [preview, setPreview] = useState<string | null>(null);
@@ -20,6 +20,12 @@ export default function ImageEditing() {
     setPrompt("");
     setResult("");
     setFinished(false);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    setPreview(URL.createObjectURL(file));
   };
 
   const handleGenerate = async () => {
@@ -52,50 +58,76 @@ export default function ImageEditing() {
   };
 
   return (
-    <div className="flex flex-col gap-4 items-end">
-      <div className="flex justify-between w-145">
+    <div className="flex flex-col gap-4 items-end w-145">
+      <div className="flex justify-between w-full">
         <div className="flex items-center gap-2">
           <ArticleGeneratorIcon />
           <p>Image Editor</p>
         </div>
         <ReloadButton disabled={!finished} onClick={handleReload} />
       </div>
+      <p className="text-[#71717A] font-normal text-[14px] w-full">
+        Upload a photo and provide a prompt, AI will edit your image.
+      </p>
 
-      <div className="flex justify-start w-145">
-        <p className="text-[#71717A] font-normal text-[14px]">
-          Upload a photo and provide a prompt, AI will edit your image.
-        </p>
+      <div className="flex-col flex gap-2 w-full">
+        <label className="flex h-10 items-center px-3 py-2 border rounded-md border-[#E4E4E7] font-medium text-[14px] cursor-pointer">
+          <p className="px-2">Choose file</p>
+          <p className="font-normal text-[#71717A]">JPG, PNG</p>
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleFileChange}
+            disabled={loading}
+          />
+        </label>
+
+        {preview && (
+          <div className="relative w-52 h-35.25 border-[#E4E4E7] rounded-lg border flex justify-center items-center">
+            <img
+              src={preview}
+              alt="preview"
+              className="w-50 h-33.25 rounded-lg object-contain"
+            />
+            <DeleteButton onClick={() => setPreview(null)} />
+          </div>
+        )}
       </div>
 
-      <FileUpload preview={preview} setPreview={setPreview} loading={loading} />
       <Textarea
-        className="w-145 h-21 px-3 py-2 border rounded-md border-[#E4E4E7] text-[#71717A] font-normal text-[14px]"
+        className="w-full h-21 px-3 py-2 border rounded-md border-[#E4E4E7] text-[#71717A] font-normal text-[14px]"
         placeholder="Your prompt here"
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
         disabled={loading}
       />
+
       <Button
         onClick={handleGenerate}
         disabled={!prompt || loading || finished}
       >
         {loading ? "Generating..." : "Generate"}
       </Button>
-      <div className="h-41 w-145 flex flex-col gap-2 items-start">
+
+      <div className="h-41 w-full flex flex-col gap-2 items-start">
         <div className="flex items-center gap-2">
-          <ImageIcon/>
+          <ImageIcon />
           <p>Result</p>
         </div>
+
         {!result && (
           <p className="text-[#71717A] font-normal text-[14px]">
             Your image. Your text. Instant transformation.
           </p>
         )}
+
         {loading && (
           <div className="flex justify-center items-center">
-            <div className="animate-spin h-6 w-6  border-2 border-gray-300 border-t-black rounded-full" />
+            <div className="animate-spin h-6 w-6 border-2 border-gray-300 border-t-black rounded-full" />
           </div>
         )}
+
         {result && (
           <div className="flex flex-col gap-1 border border-[#E4E4E7] p-4 rounded-lg">
             <Image
